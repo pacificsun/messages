7798740965dd6d96ce9e64ab39a11bb8aff18cd4
pacificsuraj <stsurajst@gmail.com>
2021-09-01 11:59:51 +0545

    for validation pipe added

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto'
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    listMessages(){}
+
+    @Post()
+    createMessage(@Body() body: CreateMessageDto){
+
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string){
+
+    }
+
+}
