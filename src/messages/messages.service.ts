15b9a437368ddf94bfbf4db7df92a73f3bbf4239
pacificsuraj <stsurajst@gmail.com>
2021-09-01 23:13:22 +0545

    di

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common'
+import { MessagesRepository } from './messages.repository'
+@Injectable()
+export class MessagesService {
+
+    constructor(public messagesRepo: MessagesRepository){
+    } 
+
+    findOne(id:string){
+        return this.messagesRepo.findOne(id)
+    }
+    create(content:string){
+        return this.messagesRepo.create(content) 
+    }
+    findAll(){
+        return this.messagesRepo.findAll() 
+    }
+
+    
+    
+    
+}
